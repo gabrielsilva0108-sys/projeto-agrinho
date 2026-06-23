7acc382313eec65395e6f60d7b7ff357d9ee9f1d
gabrielsilva0108-sys <gabriel.silva0108@escola.pr.gov.br>
2026-06-23 11:10:28 -0300

    Add function to display message in HTML

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+function mostrarMensagem() {
+  const texto = document.getElementById("texto");
 
+  texto.innerHTML = "🌱 O campo e a cidade dependem um do outro! O campo produz alimentos e a cidade oferece tecnologia e serviços.";
+}
